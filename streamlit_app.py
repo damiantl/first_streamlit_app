187860d125795e7b1bd6783ec86abde884c42f94
Damián García Castro <121572867+damiantl@users.noreply.github.com>
2022-12-29 16:55:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,12 +31,14 @@ streamlit.dataframe(fruits_to_show)
 #******************************
 
 # New section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-
 import requests
 
+# Write new header
+streamlit.header("Fruityvice Fruit Advice!")
+
 # Add entry text box
-streamlit.write('User entered ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 # GET request
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
