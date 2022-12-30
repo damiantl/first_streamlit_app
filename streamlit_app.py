abae5e2e828b47affc9772d654acc3db22708cc7
Damián García Castro <121572867+damiantl@users.noreply.github.com>
2022-12-30 11:24:33 +0100

    Update streamlit_app.py
    Added connection close

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,6 +60,7 @@ streamlit.header("The fruit load list contains:")
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 # Including Try/Except with nested If/Else
@@ -84,5 +85,6 @@ add_my_fruit = streamlit.text_input("What fruit would you like to add?")
 
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  back_from_function = insert_row_to_snowflake(add_my_fruit)
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)  
