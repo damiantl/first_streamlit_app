c61b2eda0b2a0da5d447cf27ce82b8ebfa412a29
Damián García Castro <121572867+damiantl@users.noreply.github.com>
2022-12-30 10:58:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,13 @@ streamlit.dataframe(fruits_to_show)
 #******************************
 # Call API from Streamlit
 #******************************
+# Query some data from Snowflake table
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.text("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 
 # Add function
 def get_fruityvice_data(this_fruit_choice):
