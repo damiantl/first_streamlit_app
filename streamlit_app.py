d2751de06cc9de713aefe92bb29f07215853528a
Damián García Castro <121572867+damiantl@users.noreply.github.com>
2022-12-30 10:00:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ import snowflake.connector
 #streamlit.text(my_data_row)
 
 # Query some data from Snowflake table
-my_cnx = streamlit.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
